5a80fdf2566d48bc7bebacb03cdf272a84acaa32
Clément Prévost <clementprevost45@gmail.com>
2022-12-27 19:59:53 +0100

    interfaceMatchChange

--- components/contentPage/matchList/match.vue
+++ components/contentPage/matchList/match.vue
@@ -84,12 +84,11 @@ export default {
 
         } 
 
-        text-shadow: 1px 1px 0 rgba(0, 0, 0), 
-                    -1px 1px 0 rgba(0, 0, 0),
-                    1px -1px 0 rgba(0, 0, 0),
-                    -1px -1px 0 rgba(0, 0, 0),
-                    0px 4px 4px rgba(2, 28, 41, 0.25)
-                    ;
+        font-weight: 700;
+        font-size: 18px;
+        line-height: 16px;
+        -webkit-text-stroke: 1px #021C29;
+        text-shadow:  0px 4px 4px rgba(2, 28, 41, 0.25);
 
       }
 
