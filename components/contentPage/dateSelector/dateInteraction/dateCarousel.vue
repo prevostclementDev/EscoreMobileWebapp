950329487a6d351c76392374cfe5c8e028f73567
Clément Prévost <clementprevost45@gmail.com>
2023-01-07 14:45:29 +0100

    dateSelectorUpdate

--- components/contentPage/dateSelector/dateInteraction/dateCarousel.vue
+++ components/contentPage/dateSelector/dateInteraction/dateCarousel.vue
@@ -1,26 +1,18 @@
 <template>
     <div class="dateCarousel">
         <div class="buttonDate buttonPrev">
-          <button>
+          <button @click.prevent="SetCalendar(false,'back')">
             <img src="~/assets/img/arrowCarousel.svg" alt="">
           </button>
         </div>
         <div class="containDate">
-          <div class="date">
-            <div class="day">16</div>
-            <div class="month">Sept</div>
-          </div>
-          <div class="date active">
-            <div class="day">17</div>
-            <div class="month">Sept</div>
-          </div>
-          <div class="date">
-            <div class="day">18</div>
-            <div class="month">Sept</div>
+          <div v-for="(date, index) in activeLayoutDate" class="date" v-bind:class = "(index == getActiveDate.index)?'active':''" @click.prevent="SetActiveDate(date,index)">
+            <div class="day">{{ date.toLocaleString('fr-FR',{day : 'numeric'}) }}</div>
+            <div class="month">{{ date.toLocaleString('fr-FR',{month : 'short'}).slice(0,-1) }}</div>
           </div>
         </div>
         <div class="buttonDate buttonNext">
-          <button>
+          <button @click.prevent="SetCalendar(false)">
             <img src="~/assets/img/arrowCarousel.svg" alt="">
           </button>
         </div>
@@ -28,8 +20,50 @@
 </template>
 
 <script>
+
 export default {
-  name: 'carouselDate'
+  name: 'carouselDate',
+  computed : {
+
+    activeLayoutDate() {
+
+      return this.$store.state.matchList.activeLayoutDate;
+
+    },
+
+    getActiveDate() {
+
+      return this.$store.state.matchList.activeDate;
+
+    }
+
+  },
+  methods : {
+
+    SetCalendar(now = true, changeTo = "next") {
+
+      this.$store.commit('matchList/setCalendar',{
+        now : now,
+        changeTo : changeTo,
+      })
+
+    },
+
+    SetActiveDate(date,index) {
+
+      this.$store.commit('matchList/setActiveDate',{
+        date : date,
+        index : index,
+      });
+
+    },
+
+  },
+  mounted() {
+
+      this.SetCalendar();
+
+  }
 }
 </script>
 
@@ -66,6 +100,7 @@ export default {
       font-size: 12px;
       line-height: 11px;
 
+      cursor: pointer;
 
       &.active {
 
--- store/matchList.js
+++ store/matchList.js
@@ -9,8 +9,14 @@ export const state = () => ({
         'lol': 'League of Legends',
         'rl': 'Rocket League',
         'valorant': 'Valorant',
-    }
+    },
   
+    activeDate : {
+      date: new Date(),
+      index: 1,
+    },
+    activeLayoutDate : []
+
   })
   
   export const mutations = {
@@ -29,5 +35,48 @@ export const state = () => ({
       }
   
     },
+
+    setCalendar(state, data) {
+
+      let date = new Date();
+
+      if ( !data.now ) {
+
+        date = new Date(state.activeLayoutDate[1].getTime());
+
+        if ( data.changeTo == "next") {
+
+          date.setDate(date.getDate() + 1);
+  
+        } else if ( data.changeTo == "back") {
+  
+          date.setDate(date.getDate() - 1);
+  
+        }
+
+      }
+
+      let nextDate = new Date(date.getTime());
+      let LastDate = new Date(date.getTime());
+      nextDate.setDate(nextDate.getDate() + 1);
+      LastDate.setDate(LastDate.getDate() - 1);
+
+      state.activeLayoutDate = [
+        LastDate,
+        date,
+        nextDate,
+      ]
+
+      state.activeDate.date = state.activeLayoutDate[state.activeDate.index];
+
+    },
+
+    setActiveDate(state, data) {
+
+      state.activeDate.date = data.date;
+      state.activeDate.index = data.index;
+
+    }
+
   
   }
